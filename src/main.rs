0602ea18e238286d6ea59d94ecfd8023a5430352
notnotmelon <zacharypicco@gmail.com>
2024-08-10 15:26:03 -0500

    chore: cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -170,7 +170,7 @@ struct Cli {
 
 fn main() -> Result<()> {
     let cli = Cli::parse();
-    
+
     let (stdout_read, _) = create_pipe()?;
     let mut reader = unsafe { std::fs::File::from_raw_handle(stdout_read.0) };
 
